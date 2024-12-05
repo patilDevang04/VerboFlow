4c2566b3733fdf0b2db6a7bfbef82c1ec08e7275
patilDevang04 <patildevang04@gmail.com>
2024-12-05 13:05:37 +0530

    feat able to transcribe uploaded audio

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
+import { useState ,useRef, useEffect} from 'react'
 import Header from './components/Header'
 import HomePage from './components/HomePage'
 import FileDisplay from './components/FileDisplay'
-
+import { MessageTypes } from './utils/constants'
 
 function App() {
 
@@ -16,10 +16,60 @@ function App() {
     setAudioStream(null);
   }
 
+  const worker = useRef(null)
+
+  useEffect(() => {
+    if (!worker.current) {
+      worker.current = new Worker(new URL('./utils/transcribe.worker.js', import.meta.url), {
+        type: 'module'
+      })
+    }
+
+    const onMessageReceived = async (e) => {
+      switch (e.data.type) {
+        case 'DOWNLOADING':
+          console.log('DOWNLOADING')
+          break;
+        case 'LOADING':
+          console.log('LOADING')
+          break;
+        case 'RESULT':
+          console.log(e.data.results)
+          break;
+        case 'INFERENCE_DONE':
+          console.log("DONE")
+          break;
+      }
+    }
+
+    worker.current.addEventListener('message', onMessageReceived)
+
+    return () => worker.current.removeEventListener('message', onMessageReceived)
+  })
+
+  async function readAudioFrom(file) {
+    const sampling_rate = 16000
+    const audioCTX = new AudioContext({ sampleRate: sampling_rate })
+    const response = await file.arrayBuffer()
+    const decoded = await audioCTX.decodeAudioData(response)
+    const audio = decoded.getChannelData(0)
+    return audio
+  }
+
   async function handleFormSubmission() {
     if (!file && !audioStream) { return }
 
     console.log('submission completed ...');
+
+    let audio = await readAudioFrom(file ? file : audioStream)
+
+    const model_name = `openai/whisper-tiny.en`
+
+    worker.current.postMessage({
+      type: MessageTypes.INFERENCE_REQUEST,
+      audio,
+      model_name
+    })
   }
 
   return (
